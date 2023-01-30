2738b6933b744475d6aafffcc1e98d385903df10
Rhuan Costanzo de Souza <rhuancostanzo@hotmail.com>
2023-01-29 23:00:07 -0300

    feat: secao 4 interpolation

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,22 +3,11 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-root',
   template: `
-  <app-title *ngIf="destruir"></app-title>
-  <button (click)="destruirComponent()">Destruir Componente</button>
+  <app-data-binding></app-data-binding>
   <router-outlet></router-outlet>`
 })
 export class AppComponent implements OnInit{
-  public destruir: boolean = true;
   constructor(){}
   ngOnInit(): void {
-   setTimeout(() => {
-    console.log(1);
-   }, 5000);
-  }
-
-  public destruirComponent() {
-    this.destruir = false;
   } 
- 
-    
 }
---
+++ src/app/data-binding/data-binding.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-data-binding',
+  templateUrl: './data-binding.component.html',
+  styleUrls: ['./data-binding.component.scss']
+})
+export class DataBindingComponent {
+
+  public nome: string = "Rhuan"
+  public idade: number = 32;
+}
